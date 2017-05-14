8755326cff77078ec6635879f3058987cd6d2419
CertifiedWizFul <mateo.escamilla@g.austincc.edu>
2017-05-13 21:10:51 -0500

    Updated index.php

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
-index.php
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $from = 'From: BastropThenandNow'; 
+    $to = 'mateo@g.austincc.edu'; 
+    $subject = 'Contact';
+
+    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+?>
+
+<?php
+if ($_POST['submit']) {
+    if (mail ($to, $subject, $body, $from)) { 
+        echo '<p>Your message has been sent!</p>';
+    } else { 
+        echo '<p>Something went wrong, go back and try again!</p>'; 
+    }
+}
+?>
+
